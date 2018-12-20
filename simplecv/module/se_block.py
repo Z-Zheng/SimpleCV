27ce06b2919f0f1dfe4524fdb39703f1f6966a63
zhengzhuo <zhengzhuo_git@163.com>
2018-12-20 12:08:10 +0800

    add SE block

---
+++ simplecv/module/se_block.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+from simplecv.module import GlobalAvgPool2D
+
+
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, reduction_ratio):
+        super(SEBlock, self).__init__()
+        self.gap = GlobalAvgPool2D()
+        self.seq = nn.Sequential(
+            nn.Linear(in_channels, in_channels // reduction_ratio),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_channels // reduction_ratio, in_channels),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        v = self.gap(x)
+        score = self.seq(v.view(v.size(0), v.size(1)))
+        y = x * score.view(score.size(0), score.size(1), 1, 1)
+        return y
