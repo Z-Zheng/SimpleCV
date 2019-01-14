30cdfd2a5c8ec87575f4acb05ad8e871574b24f8
zhengzhuo <zhengzhuo_git@163.com>
2019-01-14 21:50:06 +0800

    add random crop

--- simplecv/data/_np_preprocess.py
+++ simplecv/data/_np_preprocess.py
@@ -39,3 +39,34 @@ def _np_mean_std_normalize(image, mean=(123.675, 116.28, 103.53), std=(58.395, 5
     shape[-1] = -1
     return (image - mean.reshape(shape)) / std.reshape(shape)
 
+
+def _np_random_crop(image, crop_size):
+    """
+
+    Args:
+        image: 3-D tensor of shape [h, w, c]
+        crop_size: a tuple of (crop_h, crop_w)
+
+    Returns:
+
+    """
+    im_h, im_w, _ = image.shape
+    c_h, c_w = crop_size
+
+    pad_h = c_h - im_h
+    pad_w = c_w - im_w
+    if pad_h > 0 or pad_w > 0:
+        image = np.pad(image, [[0, pad_h], [0, pad_w], [0, 0]], mode='constant', constant_values=0)
+    im_h, im_w, _ = image.shape
+
+    y_lim = im_h - c_h + 1
+    x_lim = im_w - c_w + 1
+    ymin = np.random.randint(0, y_lim, 1)
+    xmin = np.random.randint(0, x_lim, 1)
+
+    xmax = xmin + c_w
+    ymax = ymin + c_h
+
+    crop_im = image[ymin:ymax, xmin:xmax, :]
+
+    return crop_im
