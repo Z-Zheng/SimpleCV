7740cd3729bd2f4998261ccf40b23a4773c85f62
Z.Zheng <zhengzhuo_git@163.com>
2019-03-12 00:20:34 +0800

    support checkpoint in resnet

---
+++ simplecv/data/distributed.py
@@ -0,0 +1,39 @@
+import math
+import torch
+from torch.utils.data.distributed import DistributedSampler
+
+
+class StepDistributedSampler(DistributedSampler):
+    def __init__(self, dataset, num_replicas=None, rank=None):
+        super(StepDistributedSampler).__init__(dataset, num_replicas, rank)
+        self.step = 0
+
+    def set_step(self, step):
+        self.step = step
+
+
+class StepDistributedRandomSubsetSampler(StepDistributedSampler):
+    def __init__(self, indices, num_replicas=None, rank=None):
+        super(StepDistributedRandomSubsetSampler, self).__init__(None, num_replicas, rank)
+
+        self.indices = indices
+        self.num_samples = int(math.ceil(len(self.indices) * 1.0 / self.num_replicas))
+
+    def __iter__(self):
+        # deterministically shuffle based on step
+        g = torch.Generator()
+        g.manual_seed(self.step)
+        indices = [self.indices[i] for i in torch.randperm(len(self.indices), generator=g)]
+
+        # add extra samples to make it evenly divisible
+        indices += indices[:(self.total_size - len(indices))]
+        assert len(indices) == self.total_size
+
+        # subsample
+        indices = indices[self.rank:self.total_size:self.num_replicas]
+        assert len(indices) == self.num_samples
+
+        return iter(indices)
+
+    def __len__(self):
+        return self.num_samples
