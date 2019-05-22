c669fba1e91fba30ee4add0044a7fd230c2f9df2
zhengzhuo <zhengzhuo_git@163.com>
2019-05-22 11:28:17 +0800

    upgrade to 0.2.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 
 install_requires = [
     'numpy>=1.11.1',
-    'tensorboardX',
+    'tensorboardX==1.7',
 ]
 setup(
     name='simplecv',
-    version='0.2.0',
+    version='0.2.1',
     description='Simplify training, evaluation, prediction in Pytorch',
     keywords='computer vision using pytorch 1.0',
     packages=find_packages(),
