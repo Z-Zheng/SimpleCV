bf87e5af0389b4aa41a328f083e36f14f0b95f41
zhengzhuo <zhengzhuo_git@163.com>
2019-07-25 17:23:37 +0800

    add fused adam

--- simplecv/opt/optimizer.py
+++ simplecv/opt/optimizer.py
@@ -3,7 +3,11 @@ from simplecv.util import registry
 
 registry.OPT.register('sgd', torch.optim.SGD)
 registry.OPT.register('adam', torch.optim.Adam)
-
+try:
+    from apex.optimizers import FusedAdam
+    registry.OPT.register('fused_adam', FusedAdam)
+except ImportError:
+    print('Please install apex for FusedAdam.')
 
 def make_optimizer(config, params):
     opt_type = config['type']
