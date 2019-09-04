5474241ce3f55ab717b838eb9a63658d65196b12
zhengzhuo <zhengzhuo_git@163.com>
2019-09-04 16:56:50 +0800

    add Pipeline

--- simplecv/api/preprocess.py
+++ simplecv/api/preprocess.py
@@ -7,4 +7,6 @@ from simplecv._impl.preprocess.comm import THMeanStdNormalize2
 from simplecv._impl.preprocess.comm import THChannelFirst
 from simplecv._impl.preprocess.comm import THChannelFirst2
 
+from simplecv._impl.preprocess.comm import Pipeline
+
 from simplecv._impl.preprocess import segm
