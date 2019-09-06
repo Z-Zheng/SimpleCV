6ed2f2715c8f81ab1fec839615e85e5b0be0dac4
zhengzhuo <zhengzhuo_git@163.com>
2019-09-06 10:23:13 +0800

    update

--- simplecv/_impl/preprocess/comm.py
+++ simplecv/_impl/preprocess/comm.py
@@ -1,87 +1,35 @@
-import simplecv._impl.preprocess.function as pF
-import torch.nn as nn
-from simplecv.util.tensor_util import to_tensor
+import random
+import math
+import torch
+import torchvision
+from torchvision.transforms import functional as F
 
 
-class Pipeline(nn.Sequential):
-    def __call__(self, *inputs):
-        for module in self._modules.values():
-            if isinstance(inputs, tuple):
-                inputs = module(*inputs)
-            else:
-                inputs = module(inputs)
-        return inputs
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + "("
+        for t in self.transforms:
+            format_string += "\n"
+            format_string += "    {0}".format(t)
+        format_string += "\n)"
+        return format_string
 
 
-class FuncWrapper(nn.Module):
-    def __init__(self, fn):
-        super(FuncWrapper, self).__init__()
-        self.fn = fn
+class THMeanStdNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
 
-    def __call__(self, *input):
-        return self.fn(*input)
-
-
-class ToTensor(nn.Module):
-    def __call__(self, *input):
-        return to_tensor(input)
-
-
-class THChannelFirst(nn.Module):
-    @staticmethod
-    def _is_channel_first(tensor):
-        return tensor.size(0) <= 8
-
-    def __call__(self, image):
-        if THChannelFirst._is_channel_first(image):
+    def __call__(self, image, target=None):
+        image = F.normalize(image, mean=self.mean, std=self.std)
+        if target is None:
             return image
-        return image.permute(2, 0, 1)
-
-
-class THChannelFirst2(THChannelFirst):
-    def __call__(self, image, other):
-        return super(THChannelFirst2, self).__call__(image), other
-
-
-class THMeanStdNormalize(nn.Module):
-    def __init__(self, mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375)):
-        super(THMeanStdNormalize, self).__init__()
-        self._m = mean
-        self._s = std
-
-    def __call__(self, image):
-        image = image.float()
-        nimage = pF.th_mean_std_normalize(image, self._m, self._s)
-
-        return nimage
-
-
-class THMeanStdNormalize2(THMeanStdNormalize):
-    def __init__(self, mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375)):
-        super(THMeanStdNormalize2, self).__init__(mean, std)
-
-    def __call__(self, image, other):
-        nimage = super(THMeanStdNormalize2, self).__call__(image)
-        return nimage, other
-
-
-class THDivisiblePad(nn.Module):
-    def __init__(self, size_divisor, mask_pad_value=255, pad_to_size=None):
-        super(THDivisiblePad, self).__init__()
-        self.size_divisor = size_divisor
-        self.mask_pad_value = mask_pad_value
-        self.pad_to_size = pad_to_size
-
-    def __call__(self, image, mask=None):
-        pimage = pF.th_divisible_pad(image, self.size_divisor)
-        if self.pad_to_size is not None:
-            pimage = pF.th_pad_to_size(image, self.pad_to_size)
-
-        if mask is not None:
-            pmask = pF.th_divisible_pad(mask, self.size_divisor, value=self.mask_pad_value)
-            if self.pad_to_size:
-                pmask = pF.th_pad_to_size(pmask, self.pad_to_size, value=self.mask_pad_value)
-        else:
-            pmask = mask
-
-        return pimage, pmask
+        return image, target
