1099e1355609e9a448268e634e1fe2b48a56cfde
zhengzhuo <zhengzhuo_git@163.com>
2019-11-05 10:25:01 +0800

    add a extension for albu

--- simplecv/api/preprocess.py
+++ simplecv/api/preprocess.py
@@ -6,3 +6,5 @@ from simplecv._impl.preprocess import comm
 from simplecv._impl.preprocess import segm
 
 from simplecv._impl.preprocess import function
+# albu based
+from simplecv._impl.preprocess import albu
